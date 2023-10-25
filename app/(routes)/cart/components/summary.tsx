07e31bd928c16305bee91899a7226d8fdaf7fdc7
wubbzy25 <carlosasalas321@gmail.com>
2023-10-24 23:46:33 -0500

    cors

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -35,12 +35,20 @@ const CheckoutSummary = () => {
   }, 0);
   
   const onCheckout = async () => {
-    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
-      productIds: items.map((item) => item.id)
-    });
-
-    window.location = response.data.url;
-  }
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/checkout`, {
+        address,
+        phoneNumber,
+        paymentMethod,
+      });
+      console.log(response);
+  
+      // Procesar la respuesta de la API (puedes mostrar un mensaje de confirmación, redireccionar, etc.)
+    } catch (error) {
+      // Manejar errores en la solicitud
+      console.error('Error al procesar el formulario de pago:', error);
+    }
+  };
   
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
