35402696a576d8c30448192f32cf1ed81a58ba2f
Carlos Andres Salas Correa <83189141+wubbzy25@users.noreply.github.com>
2023-10-25 16:20:23 -0500

    Update summary.tsx

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -26,7 +26,7 @@ const CheckoutSummary = () => {
   
   const onCheckout = async () => {
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/checkout`, {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
         address,
         phoneNumber,
         paymentMethod,
